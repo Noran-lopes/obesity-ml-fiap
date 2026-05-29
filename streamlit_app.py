cc6d89a0dd50028f91aeace54370d57afd60c38a
Noran <59796126+Noran-lopes@users.noreply.github.com>
2026-05-29 16:49:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -333,7 +333,8 @@ A obesidade é resultado de fatores físicos e comportamentais combinados.
 # =========================================================
 # DASHBOARD EXECUTIVO FINAL
 # =========================================================
-elif page == "Dashboard":
+
+    elif page == "Dashboard":
 
     st.title("📊 Dashboard Executivo de Obesidade")
 
